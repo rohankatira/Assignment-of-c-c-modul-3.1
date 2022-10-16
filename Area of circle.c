07dd8e4f2a196e4f83687de482c7dcfe1f2a3ebb
rohankatira <113296231+rohankatira@users.noreply.github.com>
2022-10-16 23:39:43 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{ int r;
+	
+	printf("radius of circle: ");
+	scanf("%d",&r);
+	printf("\nArea:%f",3.14*r*r);
+}
+
---
+++ Area of tringle.c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ( ){
+	int  l,b;
+	printf("lenth: ");
+	scanf("%d",&l);
+	
+	printf("breadth: ");
+	scanf("%d",&b);
+	
+	printf("\nArea:%d",l*b);
+}
