426e9b6ea0709454bec70e551e9de4b4f737afc2
rohankatira <113296231+rohankatira@users.noreply.github.com>
2022-10-17 23:56:30 +0530

    Add files via upload

---
+++ Display This Information using printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main( ){
+	char name [10],address[20];
+	int age,birthdate;
+	printf("your name:-");
+	scanf("%s",& name);
+	
+	printf("\n your birthdate:");
+	scanf("%d",& birthdate);
+	
+	printf("\n your age:");
+	scanf("%d",& age);
+	
+	printf("\n your address:");
+	scanf("%s",& address);
+}
